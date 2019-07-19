4a908fd16b60db21501268bed6b040bdbd097291
799142139zhufei <799142139@qq.com>
2019-07-19 18:30:56 +0800

    朴素贝叶斯算法优化（新闻分类）。

--- Data_Mining/Text_Analysis/Analysis.py
+++ Data_Mining/Text_Analysis/Analysis.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-# -*- coding:utf-8 -*- #编码声明，不要忘记！
 import pandas as pd
 import jieba
 from sklearn.model_selection import train_test_split
@@ -7,101 +5,116 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-data = pd.read_table('D:/百度网盘下载/20181015/课程资料/唐宇迪-机器学习课程资料/'
-                   '机器学习算法配套案例实战/贝叶斯-新闻分类/贝叶斯-新闻分类/data/val.txt',
-                   names=['category','theme','URL','content'],
-                   encoding='utf-8')
 
-data = data.dropna()
-content = data['content'].values.tolist()
+class TF_IDF(object):
 
-content_S = []
-for line in content: # 对每条记录集进行循环遍历分词
-   semp  = jieba.lcut(line) # 其中cut返回的是一个可迭代的对象,lcut返回的是一个列表
-   if len(semp) > 1 and semp != '\r\n': # 换行符
-      content_S.append(semp)
+    def Data_Cleaning(self,cut_file_name,stop_file_name):
+        '''
+        对文件进行分词处理
+        :param file_name: 分词文件名称
+        :param stopfile_name: 停用词文件名称
+        :return:
+        '''
+        data = pd.read_table(cut_file_name,names=['category','theme','URL','content'],encoding='utf-8')
+        content = data['content'].values.tolist()
+        content_S = []
+        for line in content: # 对每条记录集进行循环遍历分词
+           semp  = jieba.lcut(line) # 其中cut返回的是一个可迭代的对象,lcut返回的是一个列表
+           if len(semp) > 1 and semp != '\r\n': # 换行符
+              content_S.append(semp)
+        df_content = pd.DataFrame({'content_S':content_S})
 
-df_content = pd.DataFrame({'content_S':content_S})
+        # 建立停用词---对于那些出现频繁但是对文本分析没有任何用处的词语进行删除
+        stop_word = pd.read_csv(stop_file_name,
+                                index_col=False,
+                                sep="\t",
+                                quoting=3,
+                                names=['stopword'],
+                                encoding='utf-8')
+        contents = df_content['content_S'].values.tolist()  # 将数据转成字符串形式
+        stopwords = stop_word['stopword'].values.tolist()  # 将数据转成字符串形式
+        contents_clean = self.drop_stopwords(contents, stopwords)
+        # 建立新的词库
+        df_train = pd.DataFrame({'contents_clean': contents_clean, 'label': data['category']})
+        label_mapping = {"汽车": 1, "财经": 2, "科技": 3, "健康": 4,
+                         "体育": 5, "教育": 6, "文化": 7, "军事": 8, "娱乐": 9, "时尚": 0}
 
-# 建立停用词---对于那些出现频繁但是对文本分析没有任何用处的词语进行删除
-stop_word = pd.read_csv('D:/百度网盘下载/20181015/课程资料/唐宇迪-机器学习课程资料/'
-                        '机器学习算法配套案例实战/贝叶斯-新闻分类/贝叶斯-新闻分类/stopwords.txt',
-                        index_col=False,
-                        sep="\t",
-                        quoting=3,
-                        names=['stopword'],
-                        encoding='utf-8')
+        df_train['label'] = df_train['label'].map(label_mapping)
 
-# 基于停用词库清洗原先以分词好的df_content库，建立新的词库
-def drop_stopwords(contents, stopwords):
-    contents_clean = []
-    all_words = []
-    for line in contents:
-        line_clean = []
-        for word in line:
-            if word in stopwords: # 如果word是停用词则跳出当前循环
-                continue
-            line_clean.append(word) # 将不是停用词的词语加入列表中
-            all_words.append(str(word))
-        contents_clean.append(line_clean)
-    return contents_clean, all_words
+        x_train, x_test, y_train, y_test = train_test_split(df_train['contents_clean'].values,
+                                                            df_train['label'].values,
+                                                            0.3,
+                                                            random_state=1)
+        # 对训练集数据进行格式变换
+        words = []
+        for line in range(len(x_train)):
+            try:
+                word = ' '.join(x_train[line])
+                words.append(word)
+            except:
+                print(line, x_train['line'])
 
-contents = df_content['content_S'].values.tolist() # 将数据转成字符串形式
-stopwords = stop_word['stopword'].values.tolist()  # 将数据转成字符串形式
-contents_clean, all_words = drop_stopwords(contents, stopwords)
+        return  words,x_train, y_train, x_test, y_test
 
-# 建立新的词库
-df_content=pd.DataFrame({'contents_clean':contents_clean})
-df_all_words=pd.DataFrame({'all_words':all_words})
+    def drop_stopwords(self,contents, stopwords):
+        '''
+        基于停用词库清洗原先已分词好的df_content库，建立新的词库
+        :param contents: 待处理分词后的词
+        :param stopwords: 停用词
+        :return:
+        '''
+        contents_clean = []
+        for line in contents:
+            line_clean = []
+            for word in line:
+                if word in stopwords: # 如果word是停用词则跳出当前循环
+                    continue
+                line_clean.append(word) # 将不是停用词的词语加入列表中
+            contents_clean.append(line_clean)
+        return contents_clean
 
-df_train=pd.DataFrame({'contents_clean':contents_clean,'label':data['category']})
 
-# lable = df_train['label'].unique() 对标签进行编码
+    def model_predict(self,words, x_test, y_train, y_test):
+        '''
+        基于模型进行训练
+        :param words:进行预处理后的分词数据
+        :param x_train: 训练标签数据集
+        :param x_test: 测试标签数据集
+        :param y_train: 训练标注数据集
+        :param y_test: 测试标注数据
+        :return:
+        '''
+        # 基于CountVectorizer贝叶斯模型进行模拟训练
+        vec = CountVectorizer(analyzer='word',max_features=4000,lowercase = False)
+        vec.fit(words)
+        classifier = MultinomialNB()
+        classifier.fit(vec.transform(words),y_train)
 
-label_mapping = {"汽车": 1, "财经": 2, "科技": 3, "健康": 4,
-                 "体育":5, "教育": 6,"文化": 7,"军事": 8,"娱乐": 9,"时尚": 0}
+        # 对训练集数据进行格式变换
+        test_words = []
+        for line in range(len(x_test)):
+            try:
+                word = ' '.join(x_test[line])
+                words.append(word)
+            except:
+                print(line,x_test['line'])
 
-df_train['label']= df_train['label'].map(label_mapping)
+        vec_score = classifier.score(vec.fit(test_words),y_test)
+        print(vec_score)
 
-x_train,y_train,x_test,y_test = train_test_split(df_train['contents_clean'].values,
-                                                 df_train['label'].values,
-                                                 0.3,
-                                                 random_state=1)
-# 对训练集数据进行格式变换
-words = []
-for line in range(len(x_train)):
-    try:
-        word = ' '.join(x_train[line])
-        words.append(word)
-    except:
-        print(line,x_train['line'])
+        # 基于TF-IDF贝叶斯进行模拟训练
+        vectorizer = TfidfVectorizer(analyzer='word', max_features=4000,  lowercase = False)
+        vectorizer.fit(words)
+        classifier_1 = MultinomialNB()
+        classifier_1.fit(vectorizer.transform(words), y_train)
+        tf_idf_score = classifier_1.score(vectorizer.transform(test_words),y_test)
+        print(tf_idf_score)
 
-# 基于CountVectorizer贝叶斯模型进行模拟训练
-vec = CountVectorizer(analyzer='word',max_features=4000,lowercase = False)
-vec.fit(words)
 
-classifier = MultinomialNB()
-classifier.fit(vec.transform(words),y_train)
+if __name__ == '__main__':
 
-# 对训练集数据进行格式变换
-test_words = []
-for line in range(len(x_test)):
-    try:
-        word = ' '.join(x_test[line])
-        words.append(word)
-    except:
-        print(line,x_test['line'])
-
-vec_score = classifier.score(vec.fit(test_words),y_test)
-print(vec_score)
-
-# 基于TF-IDF贝叶斯进行模拟训练
-vectorizer = TfidfVectorizer(analyzer='word', max_features=4000,  lowercase = False)
-vectorizer.fit(words)
-
-classifier_1 = MultinomialNB()
-classifier_1.fit(vectorizer.transform(words), y_train)
-
-tf_idf_score = classifier_1.score(vectorizer.transform(test_words),y_test)
-
-print(tf_idf_score)
+    cut_file_name = 'val.txt'
+    stop_file_name = 'stopwords.txt'
+    TI = TF_IDF()
+    words, x_train, y_train, x_test, y_test = TI.Data_Cleaning(cut_file_name,stop_file_name)
+    TI.model_predict(words,y_train, x_test, y_test)
